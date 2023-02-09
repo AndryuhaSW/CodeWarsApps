ce4a736005bdf71d335afe7a3ac47c1652495125
AndryuhaSW <sw3ze.work@gmail.com>
2023-02-09 23:22:00 +0300

    added solution "DuplicateCount"

---
+++ CodeWars/DuplicateCount.cs
@@ -0,0 +1,28 @@
+﻿namespace CodeWars;
+
+using System;
+using System.Collections.Generic;
+
+public class Kata
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine(DuplicateCount("11223"));
+        Console.WriteLine(DuplicateCount("122345688"));
+    }
+    public static int DuplicateCount(string str)
+    {
+        Dictionary<char, int> dict = new Dictionary<char, int>();
+        str = str.ToUpper();
+        foreach (char c in str)
+            if (dict.ContainsKey(c))
+                dict[c]++;
+            else
+                dict.Add(c, 1);
+        int res = 0;
+        foreach (int i in dict.Values)
+            if (i > 1)
+                res++;
+        return res;
+    }
+}
