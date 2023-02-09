5c6965c755f166942bcdf81975453a392db5b4b0
AndryuhaSW <sw3ze.work@gmail.com>
2023-02-10 00:26:14 +0300

    fix "SpinWords"

--- CodeWars/L6_SpinWords.cs
+++ CodeWars/L6_SpinWords.cs
@@ -9,4 +9,23 @@ public class L6_SpinWords
     {
         return String.Join(' ', sentence.Split(' ').Select(str => str.Length >= 5 ? new string(str.Reverse().ToArray()) : str));
     }
+    
+    //second realisation
+    /*public static string SpinWords(string sentence)
+    {
+        string[] sent = sentence.Split(' ');
+        for (int i = 0; i < sent.Length; i++)
+            if (sent[i].Length >= 5)
+                sent[i] = Reverse(sent[i]);
+        return String.Join(" ", sent);
+    }
+  
+    private static string Reverse(String str)
+    {
+        string res = String.Empty;
+        for (int i = str.Length-1; i >= 0; i--)
+            res += str[i];
+        return res;
+    }*/
+    
 }
