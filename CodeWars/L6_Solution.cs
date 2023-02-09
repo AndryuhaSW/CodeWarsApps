f97e513b66237f9f3c45b00cdf7d605d975f8bfe
AndryuhaSW <sw3ze.work@gmail.com>
2023-02-10 00:41:34 +0300

    add "Solution"

---
+++ CodeWars/L6_Solution.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Linq;
+
+namespace CodeWars;
+
+public class L6_Solution
+{
+    //dirst
+    public static int Solution(int value)
+    {
+        if (value <= 0)
+            return 0;
+        return Enumerable.Range(1, value - 1).Select(x =>
+            x % 15 == 0 ? x :
+            x % 3 == 0 ? x :
+            x % 5 == 0 ? x :
+            0).ToArray().Sum();
+    }
+    
+    //second
+    /*public static int Solution(int value)
+    {
+        if (value <= 0) return 0;
+        return Enumerable.Range(1, value - 1)
+            .Where(x => x % 3 == 0 || x % 5 == 0).Sum();
+    }*/
+}
