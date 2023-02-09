d2d3d093c0e0bd23702c2f832977a954efb9919c
AndryuhaSW <sw3ze.work@gmail.com>
2023-02-10 00:22:59 +0300

    add "find_it"

---
+++ CodeWars/L6_find_it.cs
@@ -0,0 +1,24 @@
+namespace CodeWars;
+
+using System;
+using System.Collections.Generic;
+
+class L6_find_it
+{
+    public static int find_it(int[] seq) 
+    {
+        Dictionary<int, int> dic = new Dictionary<int, int>();
+        foreach (int num in seq)
+        {
+            if (dic.ContainsKey(num))
+                dic[num]++;
+            else
+                dic.Add(num, 1);
+        }
+        foreach (int key in dic.Keys)
+            if (dic[key] % 2 != 0)
+                return key;
+        return 0;
+    }
+       
+}
